d02d24b0329be7f2c88c18168a7c81b2bd3b0e33
Goessi <qinj1994@163.com>
2019-10-04 21:15:55 +0200

    fresh

---
+++ 60001/lectures.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 20:22:24 2019
+
+@author: JingQIN
+"""
+
+# lecture 01
+
+#text = input("Type anything ...")
+#print(5*text)
+#
+#num = int(input("Type a number..."))
+#print(5*num)
+
+#x = float(input("Enter a number for x: "))
+#y = float(input("Enter a number for y: "))
+#
+#if x == y:
+#    print("x and y are equal")
+#    if y != 0:
+#        print("therefore, x / y is", x/y)
+#elif x < y:
+#    print("x is smaller")
+#else:
+#    print("y is smaller")
+#print("thanks!")
+
+#n = input("You're in the Lost forest, go left or right?")
+#while n == "right":
+#    n = input("You're in the Lost forest, go left or right?")
+#print("You got out of the Lost Forest!")
+
+
