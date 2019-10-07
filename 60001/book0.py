201031e20886ab5498a53341313dd4490bc2a9ab
Goessi <Jing.QIN94@outlook.com>
2019-10-07 20:46:24 +0200

    biu

--- 60001/book0.py
+++ 60001/book0.py
@@ -57,8 +57,84 @@ Created on Fri Oct  4 21:08:43 2019
 #    total = total + int(c)
 #print(total)
 
-s = '1.23,2.4,3.123'
-total = 0.0
-for i in s.split(','):
-    total += float(i)
-print(total)
+#s = '1.23,2.4,3.123'
+#total = 0.0
+#for i in s.split(','):
+#    total += float(i)
+#print(total)
+
+#x = int(input('Enter an integer: '))
+#ans = 0
+#while ans**3 < abs(x):
+#    ans = ans + 1
+#if ans**3 != abs(x):
+#    print(x, 'is not a perfect cube')
+#else:
+#    if x < 0:
+#        ans = -ans
+#    print('Cube root of', x, 'is', ans)
+
+#maxVal = int(input('Enter a positive integer: '))
+#i = 0
+#while i < maxVal:
+#    i = i + 1
+#print(i)
+
+# approximation solution
+#x = 36
+#epsilon = 0.01
+#step = epsilon**2
+#numGuesses = 0
+#ans = 0.0
+#while abs(ans**2 - x) >= epsilon and ans <= x:
+#    ans += step
+#    numGuesses += 1
+#print('numGuesses =', numGuesses)
+#if abs(ans**2 - x) >= epsilon:
+#    print('Failed on square root of', x)
+#else:
+#    print(ans, 'is close to square root of', x)
+
+#x = 25
+#epsilon = 0.01
+#numGuesses = 0
+#low = 0.0
+#high = max(1.0, x)
+#
+#ans = (high + low)/2.0
+#while abs(ans**2 - x) >= epsilon:
+#    print('low =', low, 'high =', high, 'ans =', ans)
+#    numGuesses += 1
+#    if ans**2 < x:
+#        low = ans
+#    else:
+#        high = ans
+#    ans = (high + low)/2.0
+#print('numGuesses =', numGuesses)
+#print(ans, 'is close to square root of', x)
+
+x = 25
+epsilon = 0.01
+numGuesses = 0
+if x < 0:
+    low = x
+    high = 0.0
+else:
+    low = 0.0
+    high = x
+
+ans = (high + low)/2.0
+
+while abs(ans**3 - x) >= epsilon:
+    print('low =', low, 'high =', high, 'ans =', ans)
+    numGuesses += 1
+    if abs(ans**3) < abs(x):
+        if x < 0:
+            low = x
+            high = ans
+        else:
+            low = ans
+            high = x
+    ans = (high + low)/2.0
+print('numGuesses =', numGuesses)
+print(ans, 'is close to cube root of', x)
--- 60001/lectures.py
+++ 60001/lectures.py
@@ -31,4 +31,58 @@ Created on Wed Sep 25 20:22:24 2019
 #    n = input("You're in the Lost forest, go left or right?")
 #print("You got out of the Lost Forest!")
 
+#haha = 'skalaljshf'
+#print(len(haha))
+#
+#s = 'abcdfgke'
+#for index in range(len(s)):
+#    if s[index] == 'i' or s[index] == 'u':
+#        print("There is an i or u")
+#        
+#for char in s:
+#    if char == 'i' or char =='u':
+#        print("There is an i or u")
+#        
+#
+#an_letters = "aefhilmnorsxAEFHILMNORSX"
+#word = input("I will cheer for you! Enter a word: ")
+#times = int(input("Enthusiasm level (1-10): "))
+#
+#i = 0
+#while i < len(word):
+#    char = word[i]
+#    if char in an_letters:
+#        print("Give me an " + char +"! " + char)
+#    else:
+#        print("Give me a " + char + "! " + char)
+#    i += 1
+#print("What does that spell?")
+#for i in range(times):
+#    print(word, "!!!")
+
+#cube = 8
+#for guess in range(abs(cube)+1):
+#    if guess**3 >= abs(cube):
+#        break
+#if guess**3 != abs(cube):
+#    print(cube, 'is not a perfect cube')
+#else:
+#    if cube < 0:
+#        guess = - guess
+#    print('Cube root of ' + str(cube) + ' is ' + str(guess))
+
+#cube = 27
+#epsilon = 0.1
+#guess = 0.0
+#increment = 0.01
+#num_guesses = 0
+#
+#while abs(guess**3 - cube) >= epsilon and guess <= cube:
+#    guess += increment
+#    num_guesses += 1
+#print('num_guesses =', num_guesses)
+#if abs(guess**3 - cube) >= epsilon:
+#    print('Failed on cube root of', cube, 'with these parameters')
+#else:
+#    print(guess, 'is close to the cube root of', cube)
 
