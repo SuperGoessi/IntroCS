4da90289072e6b2a5546eea0e53c9ec7d9023a81
Goessi <54354115+SuperGoessi@users.noreply.github.com>
2020-02-25 09:20:22 +0100

    edit in chapter0

--- CBProjects/HelloWorld/main.cpp
+++ CBProjects/HelloWorld/main.cpp
@@ -5,33 +5,11 @@ using namespace std;
 
 int main()
 {
-//    cout << "hello world!" << endl;
-//
-//    std::cin.clear();
-//    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-//    std::cin.get();
-//
-//    int x{};
+    cout << "Hello world!" << endl;
 
-//    int x{5};
-//    std::cout <<x;
-//
-//    std::cout << "Hello" << "World";
-//
-//    std::cout << " x is equal to: " << x << std::endl;
-//
-//    std::cout << "Hi!" << std::endl;
-//    std::cout << "My name is Alex." << std::endl;
-//
-//    std::cout << "x is equal to: " << x << '\n';
-//    std::cout << "And that's all, folks!\n";
-//
-//    return 0;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
 
-    std::cout << "Enter a number: ";
-    int x{ };
-    std::cin >> x;
-    std::cout << "You entered " << x << '\n';
     return 0;
-
 }
