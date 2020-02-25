1252254279204b7cd56f89c9f8e9275ad2ebbfe8
J.Qin <Jing.QIN94@outlook.com>
2020-02-25 08:28:50 +0100

    chapter 1

--- CBProjects/HelloWorld/main.cpp
+++ CBProjects/HelloWorld/main.cpp
@@ -5,11 +5,33 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+//    cout << "hello world!" << endl;
+//
+//    std::cin.clear();
+//    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+//    std::cin.get();
+//
+//    int x{};
 
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cin.get();
+//    int x{5};
+//    std::cout <<x;
+//
+//    std::cout << "Hello" << "World";
+//
+//    std::cout << " x is equal to: " << x << std::endl;
+//
+//    std::cout << "Hi!" << std::endl;
+//    std::cout << "My name is Alex." << std::endl;
+//
+//    std::cout << "x is equal to: " << x << '\n';
+//    std::cout << "And that's all, folks!\n";
+//
+//    return 0;
 
+    std::cout << "Enter a number: ";
+    int x{ };
+    std::cin >> x;
+    std::cout << "You entered " << x << '\n';
     return 0;
+
 }
